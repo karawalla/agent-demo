fa24fa13d5f4a450f753583dab3411af10963972
rkarawalla <rkarawalla@salesforce.com>
2025-01-12 01:23:45 -0800

    removed old functions

--- login.py
+++ login.py
@@ -29,23 +29,3 @@ def get_forecast(lat,lon):
 def get_temperature(weather):
     temp = weather["current"]["temp"]
     return temp
-
-def get_humidity(weather):
-    humidity = weather["current"]["humidity"]
-    return humidity
-
-def get_wind(weather):
-    wind = weather["current"]["wind_speed"]
-    return wind
-
-def get_weather_icon(weather):
-    icon = weather["current"]["weather"][0]["icon"]
-    return icon
-
-def get_weather_description(weather):   
-    description = weather["current"]["weather"][0]["description"]
-    return description
-
-def get_weather_icon_url(weather):
-    icon = weather["current"]["weather"][0]["icon"]
-    
